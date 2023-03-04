6edcfd2a8b20c8a4f5a12ee3bb6eeceec615d4b7
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-03-03 22:25:47 -0600

    arreglando detalles

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ window.addEventListener('keydown', playsound);
 function playsound(e){
 
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-const key = document.querySelector(`.key[data-key="${e.keyCode}]"`)
+const key = document.querySelector(`.key[data-key=" ${e.keyCode}]"`)
 
 
 if(!audio) return;
 
-audio.currentTime = 0
-audio.play()
-key.classList.add('playing')
+audio.currentTime = 0;
+audio.play();
+key.classList.add('playing');
 
 }
 
