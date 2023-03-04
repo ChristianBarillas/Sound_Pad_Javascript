b92df2b1747e8564d5755c774028c5e53d600cb8
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-03-03 21:59:29 -0600

    Creando interfas

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('keydown', playsound);
+
+
+
+
+function playsound(e){
+
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+const key = document.querySelector(`key[data-key${e.keyCode}]`)
+
+
+}
